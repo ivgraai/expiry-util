fb4380fc7310d3b6fc87a179c9b3bf70c51fb1ab
Gergo IVAN <ivgraai@gmail.com>
2019-07-18 12:07:59 +0100

    Fix missing file.

---
+++ screens/MainScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export default class MainScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Main'
+  };
+
+  render() {
+    return <View>
+        </View>;
+  }
+}
