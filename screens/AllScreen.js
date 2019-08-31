fc85ab2b523d7c9ff9e2b25dd03add10ffe9fecb
Gergo IVAN <ivgraai@gmail.com>
2019-08-31 14:11:35 +0100

    Update user interface.
    vol3

--- screens/AllScreen.js
+++ screens/AllScreen.js
@@ -1,43 +1,65 @@
-import React from 'react';
-import { ListView, Text, View, Image } from 'react-native';
-import DbHelper from '../DbHelper';
+import React from "react";
+import { ListView, Text, View, Image, PixelRatio } from "react-native";
+import DbHelper from "../DbHelper";
 
 export default class AllScreen extends React.Component {
   static navigationOptions = {
-    title: 'All'
+    title: "All"
   };
+  height = PixelRatio.getPixelSizeForLayoutSize(75);
 
   constructor() {
     super();
-    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-    DbHelper.selectGoods().then((result) => {
-        // console.log('Here: ' + JSON.stringify(result));
-        this.setState({
-            dataSource: this.state.ds.cloneWithRows(result._array)
-        });
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+    DbHelper.selectGoods().then(result => {
+      // console.log('Here: ' + JSON.stringify(result));
+      this.setState({
+        dataSource: this.state.ds.cloneWithRows(result._array)
+      });
     });
     this.state = {
       ds: ds,
       dataSource: ds.cloneWithRows([
-          {
-            expiry: "2019-07-01T00:00:00.000Z",
-            id: 1,
-            image: "file:///var/mobile/Containers/Data/Application/48872CFA-5B74-4E97-8518-931C31599958/Library/Caches/ExponentExperienceData/%2540ivgraai%252Fexpiry-util/ImagePicker/17712052-855A-459D-8820-5A84E9F52690.jpg",
-            name: "dummy"
-          }
-      ]),
+        {
+          expiry: "2019-07-01T00:00:00.000Z",
+          id: 1,
+          image:
+            "file:///var/mobile/Containers/Data/Application/48872CFA-5B74-4E97-8518-931C31599958/Library/Caches/ExponentExperienceData/%2540ivgraai%252Fexpiry-util/ImagePicker/17712052-855A-459D-8820-5A84E9F52690.jpg",
+          name: "dummy"
+        }
+      ])
     };
   }
 
   render() {
-      return <ListView
+    return (
+      <ListView
         dataSource={this.state.dataSource}
-        renderRow={(rowData) => <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
-        <Text style={{flex:1}}>{rowData.id}</Text>
-        <Text style={{flex:1}}>{rowData.name}</Text>
-        <Text style={{flex:1}}>{rowData.expiry}</Text>
-        <Image source={{ uri: rowData.image }} style={{ width: 100, height: 100 }} />
-        </View>}
-      />;
+        renderRow={rowData => (
+          <View style={{ flexDirection: "row", height: this.height }}>
+            <View style={{ flex: 3, alignItems: "center" }}>
+              <Image
+                source={{ uri: rowData.image }}
+                style={{ height: "90%", width: "90%", aspectRatio: 1 }}
+              />
+            </View>
+            <View
+              style={{
+                flex: 2,
+                flexDirection: "column",
+                justifyContent: "center"
+              }}
+            >
+              <Text style={{ textAlign: "center" }}>{rowData.name}</Text>
+              <Text style={{ textAlign: "center" }}>
+                {rowData.expiry.slice(0, 10)}
+              </Text>
+            </View>
+          </View>
+        )}
+      />
+    );
   }
 }
