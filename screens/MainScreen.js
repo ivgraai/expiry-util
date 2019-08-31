e17233685d733fc84f84f5bc696c3463a4862127
Gergo IVAN <ivgraai@gmail.com>
2019-08-31 10:12:38 +0100

    Refactor user interface.

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,23 +1,31 @@
-import React from 'react';
-import { Button, View, Text, TextInput, Image, TouchableOpacity, StyleSheet } from 'react-native';
-import { Notifications } from 'expo';
-import DatePicker from 'react-native-datepicker';
-import * as ImagePicker from 'expo-image-picker';
-import DbHelper from '../DbHelper';
+import React from "react";
+import {
+  Button,
+  View,
+  Text,
+  TextInput,
+  Image,
+  TouchableOpacity,
+  StyleSheet
+} from "react-native";
+import { Notifications } from "expo";
+import DatePicker from "react-native-datepicker";
+import * as ImagePicker from "expo-image-picker";
+import DbHelper from "../DbHelper";
 
 export default class MainScreen extends React.Component {
   static navigationOptions = {
-    title: 'New'
+    title: "New"
   };
-  dayOffset = (24 * 60 * 60 * 1000);
+  dayOffset = 24 * 60 * 60 * 1000;
 
   constructor(props) {
-      super(props);
-      this.state = {
-          goods: "PERISHABLE GOODS",
-          expiry: "2020-01-01",
-          photo: undefined
-      };
+    super(props);
+    this.state = {
+      goods: "PERISHABLE GOODS",
+      expiry: "2020-01-01",
+      photo: undefined
+    };
   }
 
   buttonAdd(object) {
@@ -42,7 +50,7 @@ export default class MainScreen extends React.Component {
   }
 
   buttonPick = async () => {
-    console.log('almafa');
+    console.log("almafa");
     image = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
       allowsEditing: true,
@@ -52,56 +60,114 @@ export default class MainScreen extends React.Component {
     if (!image.cancelled) {
       this.setState({ photo: image.uri });
     }
-  }
+  };
 
   render() {
     let { photo } = this.state;
-    return <View style={{flex: 1, flexDirection: 'column', justifyContent: 'space-around'}}>
-      <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
-        <TouchableOpacity activeOpacity = { .5 } onPress={this.buttonPick} style={{borderColor: 'lightgray', borderWidth: 1, height: '75%', width: '75%'}}>
-        
-        {photo ? (
-        <Image source={{ uri: photo }} style={{ width: '100%', height: '100%' }} />
-      ) : (
-        <Text style={{ textAlign:'center' }}>(CHOOSE A PHOTO)</Text>
-      )}
-        </TouchableOpacity>
-      </View>
-      <View style={{flex: 1, alignItems: 'center'}}>
-        <TextInput selectTextOnFocus={true} style={{textAlign: 'center', height: '10%', borderColor: 'lightgray', borderWidth: 1, width: '75%'}} onChangeText={(goods) => this.setState({goods})} placeholder={this.state.goods}/>
-        <Text style={{textAlign: 'center', marginTop: 15, marginBottom: 5}}>{'EXPIRATION DATE:'}</Text>
-        <View style={{flexDirection: 'row', justifyContent: 'center'}}><DatePicker
-        style={{width: '75%'}}
-        customStyles={{dateInput:{borderColor: 'lightgray'}}}
-                date={this.state.expiry}
-                mode="date"
-                format="YYYY-MM-DD"
-                confirmBtnText="Choose"
-                cancelBtnText="Cancel"
-                showIcon={false}
-                onDateChange={(date) => {this.setState({expiry: date})}} /></View>
-      <View style={{flex: 2, width: '75%', flexDirection: 'column', justifyContent: 'center'}}><TouchableOpacity
+    return (
+      <View
+        style={{
+          flex: 1,
+          flexDirection: "column",
+          justifyContent: "space-around"
+        }}
+      >
+        <View
           style={{
-            marginRight:40,
-            marginLeft:40,
-           marginTop:10,
-            paddingTop:10,
-            paddingBottom:10,
-            backgroundColor:'gray',
-            borderRadius:10,
-            borderWidth: 1,
-            borderColor: 'white'
+            flex: 1,
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center"
           }}
-          onPress={() => this.buttonAdd(this.state)}
-          underlayColor='white'>
-          <Text style={{
-        color:'white',
-        textAlign:'center',
-        paddingLeft : 10,
-        paddingRight : 10
-    }}>Add</Text>
-      </TouchableOpacity></View>
-    </View>
-    </View>;
+        >
+          <TouchableOpacity
+            activeOpacity={0.5}
+            onPress={this.buttonPick}
+            style={{
+              borderColor: "lightgray",
+              borderWidth: 1,
+              height: "75%",
+              width: "75%"
+            }}
+          >
+            {photo ? (
+              <Image
+                source={{ uri: photo }}
+                style={{ width: "100%", height: "100%" }}
+              />
+            ) : (
+              <Text style={{ textAlign: "center" }}>(CHOOSE A PHOTO)</Text>
+            )}
+          </TouchableOpacity>
+        </View>
+        <View style={{ flex: 1, alignItems: "center" }}>
+          <TextInput
+            selectTextOnFocus={true}
+            style={{
+              textAlign: "center",
+              height: "10%",
+              borderColor: "lightgray",
+              borderWidth: 1,
+              width: "75%"
+            }}
+            onChangeText={goods => this.setState({ goods })}
+            placeholder={this.state.goods}
+          />
+          <Text style={{ textAlign: "center", marginTop: 15, marginBottom: 5 }}>
+            {"EXPIRATION DATE:"}
+          </Text>
+          <View style={{ flexDirection: "row", justifyContent: "center" }}>
+            <DatePicker
+              style={{ width: "75%" }}
+              customStyles={{ dateInput: { borderColor: "lightgray" } }}
+              date={this.state.expiry}
+              mode="date"
+              format="YYYY-MM-DD"
+              confirmBtnText="Choose"
+              cancelBtnText="Cancel"
+              showIcon={false}
+              onDateChange={date => {
+                this.setState({ expiry: date });
+              }}
+            />
+          </View>
+          <View
+            style={{
+              flex: 2,
+              width: "75%",
+              flexDirection: "column",
+              justifyContent: "center"
+            }}
+          >
+            <TouchableOpacity
+              style={{
+                marginRight: 40,
+                marginLeft: 40,
+                marginTop: 10,
+                paddingTop: 10,
+                paddingBottom: 10,
+                backgroundColor: "gray",
+                borderRadius: 10,
+                borderWidth: 1,
+                borderColor: "white"
+              }}
+              onPress={() => this.buttonAdd(this.state)}
+              underlayColor="white"
+            >
+              <Text
+                style={{
+                  color: "white",
+                  textAlign: "center",
+                  paddingLeft: 10,
+                  paddingRight: 10
+                }}
+              >
+                Add
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    );
   }
 }
