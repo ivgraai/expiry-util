850f28a2ef477ba661358880be2ebe12dac58419
Gergo IVAN <ivgraai@gmail.com>
2020-05-13 13:44:53 +0200

    Resolve compatibility issues.
    vol2

--- screens/AllScreen.js
+++ screens/AllScreen.js
@@ -1,11 +1,10 @@
 import React from "react";
 import {
-  ListView,
+  FlatList,
   Text,
   View,
   Image,
-  PixelRatio,
-  ScrollView
+  PixelRatio
 } from "react-native";
 import { NavigationEvents } from "react-navigation";
 import DbHelper from "../DbHelper";
@@ -18,12 +17,8 @@ export default class AllScreen extends React.Component {
 
   constructor() {
     super();
-    const ds = new ListView.DataSource({
-      rowHasChanged: (r1, r2) => r1 !== r2
-    });
     this.state = {
-      ds: ds,
-      dataSource: ds.cloneWithRows([
+      dataSource: [
         {
           expiry: new Date("1970-01-01T00:00:00.000Z"),
           id: 1,
@@ -31,7 +26,7 @@ export default class AllScreen extends React.Component {
             "file:///var/mobile/Containers/Data/Application/48872CFA-5B74-4E97-8518-931C31599958/Library/Caches/ExponentExperienceData/%2540ivgraai%252Fexpiry-util/ImagePicker/17712052-855A-459D-8820-5A84E9F52690.jpg",
           name: "dummy"
         }
-      ])
+      ]
     };
   }
 
@@ -42,26 +37,25 @@ export default class AllScreen extends React.Component {
           onWillFocus={payload => {
             DbHelper.selectGoods().then(result => {
               this.setState({
-                dataSource: this.state.ds.cloneWithRows(
+                dataSource:
                   result._array
                     .map(a => {
                       return { ...a, expiry: new Date(a.expiry) };
                     })
                     .sort((a, b) => a.expiry.getTime() - b.expiry.getTime())
-                )
               });
             });
-            this.refs._scrollView.scrollTo(0); // Should be in the callback.
+            this.refs._scrollView.scrollToOffset({ offset: 0 }); // Should be in the callback.
           }}
         />
-        <ScrollView ref="_scrollView">
-          <ListView
-            dataSource={this.state.dataSource}
-            renderRow={rowData => (
+          <FlatList ref="_scrollView"
+            data={this.state.dataSource}
+            keyExtractor={item => item.id.toString()}
+            renderItem={({item}) => (
               <View style={{ flexDirection: "row", height: this.height }}>
-                <View style={{ flex: 3, alignItems: "center" }}>
+                <View style={{ flex: 2, alignItems: "center" }}>
                   <Image
-                    source={{ uri: rowData.image }}
+                    source={{ uri: item.image }}
                     style={{ height: "90%", width: "90%", aspectRatio: 1 }}
                   />
                 </View>
@@ -73,16 +67,15 @@ export default class AllScreen extends React.Component {
                   }}
                 >
                   <Text style={{ textAlign: "center" }}>
-                    {rowData.name.toUpperCase()}
+                    {item.name.toUpperCase()}
                   </Text>
                   <Text style={{ textAlign: "center" }}>
-                    {rowData.expiry.toLocaleDateString()}
+                    {item.expiry.toLocaleDateString()}
                   </Text>
                 </View>
               </View>
             )}
           />
-        </ScrollView>
       </View>
     );
   }
