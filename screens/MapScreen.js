49b9b61883bd9b45762e97e26d6a8b5fb5b95573
Gergo IVAN <ivgraai@gmail.com>
2020-05-14 11:59:01 +0200

    Add map activity.

---
+++ screens/MapScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+class MapScreen extends React.Component {
+    render() {
+        return (
+            <View><Text>Map Activity</Text></View>
+        );
+    }
+}
+
+export default MapScreen;
