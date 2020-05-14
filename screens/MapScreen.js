46b6bfbe5964e4b4bca1aebfb2cdc76453afe476
Gergo IVAN <ivgraai@gmail.com>
2020-05-14 12:11:49 +0200

    Introduce map view.

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,12 +1,28 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { StyleSheet, View, Dimensions } from "react-native";
+import MapView from 'react-native-maps';
 
 class MapScreen extends React.Component {
     render() {
         return (
-            <View><Text>Map Activity</Text></View>
+            <View style={styles.container}>
+                <MapView style={styles.mapStyle} />
+            </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    mapStyle: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height
+    }
+});
 
 export default MapScreen;
