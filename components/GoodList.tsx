eacc4efac8eb4fc8b21284809a34acb59f0530f2
Gergo IVAN <ivgraai@gmail.com>
2020-06-26 13:44:12 +0200

    Transform good list into a function component.
    related to #48

--- components/GoodList.tsx
+++ components/GoodList.tsx
@@ -1,51 +1,48 @@
 import React from "react";
 import { FlatList, View, Text } from "react-native";
-import { ThemeContext } from "react-navigation";
+import { useTheme } from "react-navigation";
 import CachedImage from "../components/CachedImage";
 import DbHelper from "../services/DbHelper";
 import { styles } from "../constants/styles/GoodList";
 
-class GoodList extends React.Component {
-    static contextType = ThemeContext;
-    render() {
-        const theme = this.context;
-        const withStyle = styles('dark' === theme);
-        let {innerRef, dataSource, customNodesForTheItem} = this.props;
-        let now = new Date().getTime();
-        return (
-          <FlatList ref={innerRef}
-            data={dataSource.filter((item: {expiry: Date}) => (now <= item.expiry.getTime()))}
-            keyExtractor={item => item.id.toString()}
-            renderItem={({item}) => (
-              <View style={withStyle.parent}>
-                <View style={withStyle.leftChild}>
-                  <CachedImage
-                    source={{ uri: item.image }}
-                    style={withStyle.leftChildImage}
-                    onDownloaded={(uri: string) => DbHelper.newImage(uri, true)}
-                  />
-                </View>
-                <View
-                  style={withStyle.rightChild}
-                >
-                  <View style={withStyle.grandChildHeader}>
-                    <Text style={withStyle.grandChildHeaderText}>
-                      {item.name.toUpperCase()}
-                    </Text>
-                  </View>
-                  <View style={withStyle.grandChildBody}>
-                    <Text style={withStyle.grandChildBodyText}>
-                      {item.expiry.toLocaleDateString()}
-                    </Text>
-                    { customNodesForTheItem ?
-                      customNodesForTheItem(item) : null }
-                  </View>
-                </View>
-              </View>
-            )}
-          />
-        );
-    }
+function GoodList(props: any) {
+  const theme = useTheme();
+  const withStyle = styles('dark' === theme);
+  let {innerRef, dataSource, customNodesForTheItem} = props;
+  let now = new Date().getTime();
+  return (
+    <FlatList ref={innerRef}
+      data={dataSource.filter((item: {expiry: Date}) => (now <= item.expiry.getTime()))}
+      keyExtractor={item => item.id.toString()}
+      renderItem={({item}) => (
+        <View style={withStyle.parent}>
+          <View style={withStyle.leftChild}>
+            <CachedImage
+              source={{ uri: item.image }}
+              style={withStyle.leftChildImage}
+              onDownloaded={(uri: string) => DbHelper.newImage(uri, true)}
+            />
+          </View>
+          <View
+            style={withStyle.rightChild}
+          >
+            <View style={withStyle.grandChildHeader}>
+              <Text style={withStyle.grandChildHeaderText}>
+                {item.name.toUpperCase()}
+              </Text>
+            </View>
+            <View style={withStyle.grandChildBody}>
+              <Text style={withStyle.grandChildBodyText}>
+                {item.expiry.toLocaleDateString()}
+              </Text>
+              { customNodesForTheItem ?
+                customNodesForTheItem(item) : null }
+            </View>
+          </View>
+        </View>
+      )}
+    />
+  );
 }
 
 export default React.forwardRef((props, ref) => <GoodList innerRef={ref} {...props}/>);
