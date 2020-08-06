64d5d3392ed0d5e17877bd7d4eb3c6e18b73feae
Gergo IVAN <ivgraai@gmail.com>
2020-08-06 11:07:25 +0200

    Test HttpClient::addGood

--- services/__tests__/HttpClient-test.ts
+++ services/__tests__/HttpClient-test.ts
@@ -2,30 +2,34 @@ import fetch from "jest-fetch-mock";
 import HttpClient from "../HttpClient";
 import * as Dtos from "../../constants/Dtos";
 import Constants from "expo-constants";
+import Utility from "../../common/Utility";
+
+function FormDataMock() {
+  this.append = jest.fn();
+}
+global.FormData = FormDataMock;
 
 const BASE_URL: string = Constants.manifest.extra.serverUrl;
 const TIME_TO_RUN: number = Constants.manifest.extra.httpTimeout;
 function mockOnce(body?: string, milliseconds: number = 0) {
-  return fetchMock.once(
-    () => {
-      if (0 != milliseconds) {
-        jest.advanceTimersByTime(milliseconds);
-      }
-      return new Promise((resolve) =>
-        resolve({
-          status: 200,
-          statusText: "OK",
-          url: "",
-          body,
-          init: {
-            headers: {
-              "Content-Type": "application/json",
-            },
-          },
-        })
-      );
+  return fetchMock.once(() => {
+    if (0 != milliseconds) {
+      jest.advanceTimersByTime(milliseconds);
     }
-  );
+    return new Promise((resolve) =>
+      resolve({
+        status: 200,
+        statusText: "OK",
+        url: "",
+        body,
+        init: {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        },
+      })
+    );
+  });
 }
 function testToken(
   headers: Headers | string[][] | Record<string, string> | undefined,
@@ -77,9 +81,51 @@ describe(`HttpClient`, () => {
     expect(fetch.mock.calls[counter][1]!.method).toEqual("DELETE");
     testToken(fetch.mock.calls[counter][1]!.headers, token);
   });
-  // addGood
-  test(`listAllGood`, async () => {
+  test(`addGood`, async () => {
     const counter = 3;
+    mockOnce();
+    var token = "test token",
+      name = "test name",
+      expiry = new Date(),
+      latitude = 47.497913,
+      longitude = 19.040236,
+      available = true,
+      image = Utility.convertImageToDto(
+        "file:///var/mobile/Containers/Data/Application/CB2EDF3E-81AA-4E30-9272-BF039B11AA32/Library/Caches/ExponentExperienceData/%2540ivgraai%252Fexpiry-util/ImagePicker/7106AD16-2332-4A07-AB69-1DFF7DE2F145.jpg"
+      );
+    await HttpClient.addGood(
+      token,
+      name,
+      expiry,
+      latitude,
+      longitude,
+      available,
+      image
+    );
+    expect(fetch.mock.calls[counter][0]).toEqual(BASE_URL + "good/add");
+    expect(fetch.mock.calls[counter][1]!.method).toEqual("POST");
+    testToken(fetch.mock.calls[counter][1]!.headers, token);
+    let body: any = (fetch.mock.calls[counter][1]!.body as FormData).append.mock
+      .calls;
+    expect(body[0][0]).toEqual("image");
+    expect(body[0][1].name).toEqual("7106AD16-2332-4A07-AB69-1DFF7DE2F145.jpg");
+    expect(body[0][1].type).toEqual("image/jpg");
+    expect(body[0][1].uri).toEqual(
+      "/var/mobile/Containers/Data/Application/CB2EDF3E-81AA-4E30-9272-BF039B11AA32/Library/Caches/ExponentExperienceData/%2540ivgraai%252Fexpiry-util/ImagePicker/7106AD16-2332-4A07-AB69-1DFF7DE2F145.jpg"
+    );
+    expect(body[1][0]).toEqual("name");
+    expect(body[1][1]).toEqual(name);
+    expect(body[2][0]).toEqual("expiry");
+    expect(body[2][1]).toEqual(expiry.toISOString());
+    expect(body[3][0]).toEqual("location.latitude");
+    expect(body[3][1]).toEqual(latitude);
+    expect(body[4][0]).toEqual("location.longitude");
+    expect(body[4][1]).toEqual(longitude);
+    expect(body[5][0]).toEqual("available");
+    expect(body[5][1]).toEqual(available);
+  });
+  test(`listAllGood`, async () => {
+    const counter = 4;
     var expectedName = "test name 2",
       expectedExpiry = new Date(),
       expectedIsRequestedByOther = true,
@@ -120,7 +166,7 @@ describe(`HttpClient`, () => {
     testToken(fetch.mock.calls[counter][1]!.headers, token);
   });
   test(`listNearbyGood`, async () => {
-    const counter = 4;
+    const counter = 5;
     var expectedName = "test name",
       expectedExpiry = new Date(),
       expectedDistance = 1234,
@@ -161,7 +207,7 @@ describe(`HttpClient`, () => {
     expect(fetch.mock.calls[counter][1]!.method).toEqual("GET");
   });
   test(`requestTheGood`, async () => {
-    const counter = 5;
+    const counter = 6;
     var token = "test token";
     mockOnce();
     var goodId = 1,
@@ -175,7 +221,7 @@ describe(`HttpClient`, () => {
     );
   });
   test(`checkStatus`, async () => {
-    const counter = 6;
+    const counter = 7;
     var payload = {
       address: {
         country: "test country",
@@ -202,7 +248,7 @@ describe(`HttpClient`, () => {
     expect(fetch.mock.calls[counter][1]!.method).toEqual("GET");
   });
   test(`findAllRequest`, async () => {
-    const counter = 7;
+    const counter = 8;
     var expectedUsername = "test username 1",
       expectedDatetime = new Date(),
       expectedMessage = "test message 1",
@@ -247,10 +293,10 @@ describe(`HttpClient`, () => {
     testToken(fetch.mock.calls[counter][1]!.headers, token);
   });
   test(`approveRequest`, async () => {
-    const counter = 8;
+    const counter = 9;
     var message = "test message";
     mockOnce(message);
-    var token = "token",
+    var token = "test token",
       id = 0;
     await HttpClient.approveRequest(token, id, message);
     expect(fetch.mock.calls[counter][0]).toEqual(BASE_URL + "request/" + id);
@@ -261,7 +307,7 @@ describe(`HttpClient`, () => {
   test(`aborting`, async () => {
     jest.useFakeTimers();
     mockOnce("", TIME_TO_RUN + 500);
-    await expect(HttpClient.login("dummy", "dummy")).rejects.toThrow("Aborted");
+    await expect(HttpClient.login("test name", "test password")).rejects.toThrow("Aborted");
     jest.useRealTimers();
   });
 });
